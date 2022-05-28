e3e778a142f1247f56ba170e745875b55ab56da8
Unizalin <unizalin@gmail.com>
2022-05-29 02:57:49 +0800

    update postman collection

--- routes/upload.js
+++ routes/upload.js
@@ -1,16 +1,16 @@
 var express = require('express');
 var router = express.Router();
-const fileController = require("../controllers/upload.controller");
+const uploadController = require("../controllers/upload.controller");
 const uploadImage = require('../server/image');
 const {isAuth} = require('../server/auth');
 const handleErrorAsync = require("../server/handleErrorAsync")
 
 
 /* GET home page. */
-router.get('/',fileController.allImages);
+router.get('/',uploadController.allImages);
 
 
-router.post('/', isAuth , uploadImage , handleErrorAsync(fileController.upload));
+router.post('/', isAuth , uploadImage , handleErrorAsync(uploadController.upload));
 
 
 module.exports = router;
